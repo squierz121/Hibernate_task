51ab27a2bda092ede5f6b5532235cecc7f05252d
Vyacheslav Kalinovsky <apacer121@mail.ru>
2024-12-21 13:14:22 +1000

    JDBC task

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,8 @@
 package jm.task.core.jdbc;
 
-
-import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
 
-import java.util.List;
+
 
 
 public class Main {
